f791cb62dc42c5271e3f1bee7733e81425e65e69
SebiB24 <132143076+SebiB24@users.noreply.github.com>
2026-05-07 22:32:17 +0300

    changing Task List title depending on filters

--- TodoListFrontend/src/components/TaskList.jsx
+++ TodoListFrontend/src/components/TaskList.jsx
@@ -7,6 +7,8 @@ import "./TaskList.css"
 function TaskList({ filters, update }) {
 
     const [tasks, setTasks] = useState([]);
+    const [title, setTitle] = useState("All Tasks");
+
 
     useEffect(() => {
         const fetchTasks = async () => {
@@ -17,6 +19,7 @@ function TaskList({ filters, update }) {
                 });
                 if (response) {
                     setTasks(response);
+                    setTitle(getTitle());
                 }
             } catch (error) {
                 console.error("Error fetching tasks:", error);
@@ -30,48 +33,59 @@ function TaskList({ filters, update }) {
     const priority2Tasks = tasks.filter(task => task.priority === TaskPriority.PRIORITY_2);
     const priority3Tasks = tasks.filter(task => task.priority === TaskPriority.PRIORITY_3);
 
-    return (
-        <div className="task-list-container">
-            <h1 className="main-title">TODO</h1>
-            {priority1Tasks.length > 0 && (
-                <div className="priority-section">
-                    <h2 className="priority-header">Priority 1</h2>
-                    <div className="task-group">
-                        {
-                            priority1Tasks.map((task, index) => (
-                                <TaskListItem key={index} task={task} />
-                            ))
-                        }
-                    </div>
-                </div>
-            )}
-            {priority2Tasks.length > 0 && (
-                <div className="priority-section">
-                    <h2 className="priority-header">Priority 2</h2>
-                    <div className="task-group">
-                        {
-                            priority2Tasks.map((task, index) => (
-                                <TaskListItem key={index} task={task} />
-                            ))
-                        }
-                    </div>
-                </div>
-            )}
-            {priority3Tasks.length > 0 && (
-                <div className="priority-section">
-                    <h2 className="priority-header">Priority 3</h2>
-                    <div className="task-group">
-                        {
-                            priority3Tasks.map((task, index) => (
-                                <TaskListItem key={index} task={task} />
-                            ))
-                        }
-                    </div>
-                </div>
-            )}
+    const getTitle = () => {
+        if (filters.status === TaskStatus.COMPLETE) {
+            return "Task History";
+        }
+        if (filters.today === true) {
+            return "Today's Tasks";
+        }
+        return "All Tasks";
+    }
 
-        </div>
-    )
+
+return (
+    <div className="task-list-container">
+        <h1 className="main-title">{title}</h1>
+        {priority1Tasks.length > 0 && (
+            <div className="priority-section">
+                <h2 className="priority-header">Priority 1</h2>
+                <div className="task-group">
+                    {
+                        priority1Tasks.map((task, index) => (
+                            <TaskListItem key={index} task={task} />
+                        ))
+                    }
+                </div>
+            </div>
+        )}
+        {priority2Tasks.length > 0 && (
+            <div className="priority-section">
+                <h2 className="priority-header">Priority 2</h2>
+                <div className="task-group">
+                    {
+                        priority2Tasks.map((task, index) => (
+                            <TaskListItem key={index} task={task} />
+                        ))
+                    }
+                </div>
+            </div>
+        )}
+        {priority3Tasks.length > 0 && (
+            <div className="priority-section">
+                <h2 className="priority-header">Priority 3</h2>
+                <div className="task-group">
+                    {
+                        priority3Tasks.map((task, index) => (
+                            <TaskListItem key={index} task={task} />
+                        ))
+                    }
+                </div>
+            </div>
+        )}
+
+    </div>
+)
 }
 
 export default TaskList
